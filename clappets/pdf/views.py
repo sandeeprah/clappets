d5fa9d5531d0079372c88aa019ef4fc7e42b3a16
sandeeprah <32537458+sandeeprah@users.noreply.github.com>
2017-11-30 15:26:42 +0530

    template header added

--- clappets/pdf/views.py
+++ clappets/pdf/views.py
@@ -38,8 +38,7 @@ def pdf_dbDoc(doc_id):
             'margin-right':'19mm',
             'encoding':'UTF-8',
             'print-media-type' : None,
-    #        'header-html' : 'header.html',
-#            'header-left' : 'My Header',
+#            'header-left' : 'My Static Header',
             'header-line' : None,
             'header-font-size' : '8',
             'header-font-name' : 'Calibri',
@@ -56,8 +55,9 @@ def pdf_dbDoc(doc_id):
 
         this_folderpath = os.path.dirname(os.path.abspath(__file__))
         wkhtmltopdf_path = "/" + os.path.join("home", "wkhtmltox", "bin", "wkhtmltopdf")
+        wkhtmltopdf_path = r'/home/wkhtmltox/bin/wkhtmltopdf'
+#        wkhtmltopdf_path = r'C:\Program Files\wkhtmltopdf\bin\wkhtmltopdf.exe'
         config = pdfkit.configuration(wkhtmltopdf=wkhtmltopdf_path)
-#        config = pdfkit.configuration(wkhtmltopdf=r'C:\Program Files\wkhtmltopdf\bin\wkhtmltopdf.exe')
         this_folderpath = os.path.dirname(os.path.abspath(__file__))
         css_path = os.path.join(this_folderpath, 'print.css')
 
@@ -65,68 +65,21 @@ def pdf_dbDoc(doc_id):
         context_header['title'] = title
         context_header['rev'] = rev
         context_header['doc_id'] = doc_id
-#        add_pdf_header(options, context_header=context_header)
+        add_pdf_header(options, context_header=context_header)
 
         try:
             pdf = pdfkit.from_string(main_content, False, options=options, configuration=config, css=css_path)
-#            pdf = pdfkit.from_url('http://127.0.0.1:5000/htm/document/db/1122-mec-dat-pmp-c01-1/', False ,options=options,configuration=config)
         except Exception as e:
             print(str(e))
             return (str(e))
         finally:
-#            os.remove(options['header-html'])
-            pass
+            os.remove(options['header-html'])
 
         response = build_response(pdf)
         return response
 
 
 
-
-#        return render_template(template, doc=doc)
-
-
-@app.route('/generate_pdf')
-def render_pdf_custom_header(foo, bar):
-    main_content = render_template('main_pdf.html', foo=foo)
-    options = {
-    }
-    options = {
-        'page-size' : 'A4',
-        'margin-top':'25mm',
-        'margin-bottom':'19mm',
-        'margin-left':'19mm',
-        'margin-right':'19mm',
-        'encoding':'UTF-8',
-        'print-media-type' : None,
-#        'header-html' : 'header.html',
-        'header-left' : 'My Header',
-        'header-line' : None,
-        'header-font-size' : '8',
-        'header-font-name' : 'Calibri',
-        'header-spacing' : '5',
-        'footer-left' : "template_url : http://www.clappets.com/htm/document/tpl/mec/dat/pmp/c01/",
-        'footer-line' : None,
-        'footer-font-size' : '8',
-        'footer-font-name' : 'Calibri',
-        'footer-spacing' : '5',
-        'disable-smart-shrinking' : None,
-        '--encoding': "utf-8"
-    }
-
-
-#    add_pdf_header(options, bar)
-#    add_pdf_footer(options)
-
-    try:
-        pdf = pdfkit.from_string(main_content, False, options=options)
-    finally:
-        pass
-#        os.remove(options['--header-html'])
-#        os.remove(options['--footer-html'])
-
-    response = build_response(pdf)
-    return response
 
 def add_pdf_header(options, context_header):
     with tempfile.NamedTemporaryFile(suffix='.html', delete=False) as header:
