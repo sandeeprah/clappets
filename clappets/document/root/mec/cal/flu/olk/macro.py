fd1c0e6b949179ec1c7665c14d857f0ae1ec2ce7
sandeeprah <raheja.sandeep@gmail.com>
2018-07-01 07:52:59 +0530

    xhtml errors updated

---
+++ clappets/document/root/mec/cal/flu/olk/macro.py
@@ -0,0 +1,34 @@
+from math import *
+from copy import deepcopy
+from clappets.units import treeUnitConvert, SI_UNITS
+from clappets.utils import parseFloat, roundit
+from techclappets.fluids import orifice as orf
+
+def calculate(doc_original):
+    doc = deepcopy(doc_original)
+    treeUnitConvert(doc, doc['units'], SI_UNITS)
+    doc['errors'] = []
+
+    Pi = parseFloat(doc['input']['Pi']['_val'])
+    Pe = parseFloat(doc['input']['Pe']['_val'])
+    Ti = parseFloat(doc['input']['Ti']['_val'])
+    MW = parseFloat(doc['input']['MW']['_val'])
+    k = parseFloat(doc['input']['k']['_val'])
+    A = parseFloat(doc['input']['A']['_val'])
+    Cd = parseFloat(doc['input']['Cd']['_val'])
+
+
+    r = Pe/Pi
+    f = k/(k-1)
+
+    rho0 = orf.density(Pi,Ti,MW)
+    v, G, isChoked = orf.mflow_reservoir_orifice(Pi, Ti, Pe, MW, k, A, Cd)
+    Q = A*v
+    doc['result'].update({'v':{'_val' : str(roundit(v)), '_dim':'speed'}})
+    doc['result'].update({'Q':{'_val' : str(roundit(Q)), '_dim':'flow'}})
+    doc['result'].update({'G':{'_val' : str(roundit(G)), '_dim':'massflow'}})
+
+    treeUnitConvert(doc, SI_UNITS, doc['units'], autoRoundOff=True)
+    doc_original['result'].update(doc['result'])
+    doc_original['errors'] = doc['errors']
+    return True
--- testrig.py
+++ testrig.py
@@ -1,5 +1,5 @@
 # import the run_tests from relevant test module
-from clappets.document.root.mec.cal.ems.cnv.test  import run_tests
+from clappets.document.root.mec.cal.flu.olk.test  import run_tests
 
 #from techclappets.electrical.cable.cablesizing import run_tests
 
